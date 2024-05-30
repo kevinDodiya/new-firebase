41dc7c4c66b2bd11330c847e4af4b6c66b364427
kevinDodiya <kevindodiya75@gmail.com>
2024-05-30 20:41:01 +0530

    add envirment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
+    apiKey: "AIzaSyBAauUibkX2ANOI3BK3Vk59CxCSZVvIPE8",
+    authDomain: "fir-connection-93e1c.firebaseapp.com",
+    databaseURL: "https://fir-connection-93e1c-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "fir-connection-93e1c",
+    storageBucket: "fir-connection-93e1c.appspot.com",
+    messagingSenderId: "593719121932",
+    appId: "1:593719121932:web:bc5add8c647f9e7e27f629"
 
   }
 };
